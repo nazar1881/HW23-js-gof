cc4dfa026327354a3c16cf026aa4765fecc5111d
nazar1881 <90273320+nazar1881@users.noreply.github.com>
2022-01-30 10:36:22 +0200

    second commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-console.log("/////////////////////Перший варіант////////////////////////");
-
 (function ($) {
   var o = $({});
   $.each({
@@ -62,61 +60,3 @@ rose.subscribe()
 billy.subscribe();
 jack.subscribe()
 jack.publish();
-
-//////////////////////////////////////
-/////////////////////////////////////
-////////////////////////////////////
-
-console.log("/////////////////////Другий варіант через Медіатор/////////////////////");
-
-class Bear {
-  constructor(name) {
-    this.name = name;
-    this.room = null;
-  }
-
-  send(message, to) {
-    this.room.send(message, this, to);
-  }
-
-  receive(message, from) {
-    console.log(`${from.name} to ${this.name}: ${message}`);
-  }
-}
-
-class Chat {
-  constructor() {
-    this.users = {};
-  }
-
-  register(user) {
-    this.users[user.name] = user;
-    user.room = this;
-  }
-
-  send(message, from, to) {
-    if (to) {
-      to.receive(message, from);
-    } else {
-      Object.keys(this.users).forEach(key => {
-        if (this.users[key] !== from) {
-          this.users[key].receive(message, from);
-        }
-      })
-    }
-  }
-}
-
-const billyMeditor = new Bear('Billy');
-const jackMeditor = new Bear('Jack');
-const roseMeditor = new Bear('Rose');
-
-const room = new Chat();
-
-room.register(billyMeditor);
-room.register(jackMeditor);
-room.register(roseMeditor);
-
-jackMeditor.send('Hello Rose, i like you', roseMeditor);
-roseMeditor.send('Hello Billy', billyMeditor);
-billyMeditor.send('I need to run, bye', roseMeditor);
